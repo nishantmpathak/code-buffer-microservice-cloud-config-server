731b376c7f0a9fc9d9961a787ee71969b48dce79
Nishant Pathak <nishantpathak97@gmail.com>
2022-04-17 22:47:45 +0530

    first commit

---
+++ src/test/java/com/asymptote/could/CouldConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asymptote.could;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouldConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
